b044b5dcf20a9f08d3dad4c315b92ac1eb9e8fe5
Em Grasmeder <em@gras.org>
2022-12-04 18:52:13 +0100

    add books, change filetype

--- add-book.clj
+++ add-book.clj
@@ -4,4 +4,4 @@
       subtitle (do (prn "Subtitle:") (read-line))
       author (do (prn "Author:") (read-line))
       isbn (do (prn "ISBN-13:") (read-line))]
-   (spit "db.csv" (format "%s\t%s\t%s\t%s\n" title subtitle author isbn) :append true))
+   (spit "db.tsv" (format "%s\t%s\t%s\t%s\n" title subtitle author isbn) :append true))
